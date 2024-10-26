e169f22b169add5ea5f7ddd5d02ff7e3f7dfcb13
jonathas <jonathaspereira139@gmail.com>
2024-10-26 12:34:29 -0300

    video 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatToolbarModule} from '@angular/material/toolbar';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CoursesComponent } from './cpurses/courses/courses.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CoursesComponent
+
 
   ],
   imports: [
