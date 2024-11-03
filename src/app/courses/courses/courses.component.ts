7d6f070651f3f7f7de481a5bb072daa59f5ecb5d
jonathas <jonathaspereira139@gmail.com>
2024-11-03 08:53:20 -0300

    aula10

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -10,7 +10,7 @@ import { Observable } from 'rxjs';
 })
 export class CoursesComponent {
 
-  courses: Observable<Course[]>;
+  courses$: Observable<Course[]>;
 
   displayedColumns = ['name','category'];
 
@@ -19,7 +19,7 @@ export class CoursesComponent {
   constructor(private coursesService: CoursesService ){
     //this.courses = [];
     //this.coursesService = new CoursesService();
-    this.courses = this.coursesService.list();
+    this.courses$ = this.coursesService.list();
   }
 
 }
--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Course } from '../model/course';
 import { HttpClient } from '@angular/common/http';
-import { tap } from 'rxjs';
+import { delay, first, tap } from 'rxjs';
 
 
 @Injectable({
@@ -16,6 +16,8 @@ export class CoursesService {
   list() {
     return this.httpClient.get<Course[]>(this.API)
     .pipe(
+      first(),
+      delay(500),
       tap(courses => console.log(courses))
     );
   }
