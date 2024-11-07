9f5836179be0696de2b9d248d99035f7f2375dda
jonathas <jonathaspereira139@gmail.com>
2024-11-07 20:53:09 -0300

    aula11 final

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
 import { catchError, Observable, of } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from '../../shared/components/error-dialog/error-dialog.component';
 
 @Component({
   selector: 'app-courses',
@@ -16,16 +18,26 @@ export class CoursesComponent {
 
   //coursesService: CoursesService;
 
-  constructor(private coursesService: CoursesService ){
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog
+  ){
     //this.courses = [];
     //this.coursesService = new CoursesService();
     this.courses$ = this.coursesService.list()
     .pipe(
       catchError(error => {
-        console.log(error)
+        this.onError('Erro ao carregar cursos.');
         return of([])
       })
     );
+  }
+
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
   }
 
 }
