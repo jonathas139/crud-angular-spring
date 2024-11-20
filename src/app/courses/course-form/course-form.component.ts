8cb732f6ae0f9925d60f6a89e9c8cc49229827b7
jonathas <jonathaspereira139@gmail.com>
2024-11-20 15:44:39 -0300

    aula21f

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,6 +1,7 @@
+import { Location } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import {MatSnackBar} from '@angular/material/snack-bar';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { CoursesService } from '../services/courses.service';
 
@@ -16,7 +17,8 @@ export class CourseFormComponent {
   private snackBar = inject(MatSnackBar);
 
   constructor(private formBuilder: FormBuilder,
-    private service: CoursesService) {
+    private service: CoursesService,
+    private location: Location) {
     this.form = this.formBuilder.group({
       name:[null],
       category: [null]
@@ -25,14 +27,19 @@ export class CourseFormComponent {
 
   onSubmit() {
     this.service.save(this.form.value).subscribe({
-      next: (result) => console.log(result),
+      next: (result) => this.onSuccess(),
       error: () => this.onError()});
   }
 
   onCancel(){
+    this.location.back();
   }
 
+  private onSuccess(){
+    this.snackBar.open('Criado com Sucesso!', '', { duration: 5000 });
+    this.onCancel();
+  }
   private onError(){
-    this.snackBar.open('Erro ao Salvar Curso!', '', { duration: 5000 });
+    this.snackBar.open('Erro ao Salvar!', '', { duration: 5000 });
   }
 }
